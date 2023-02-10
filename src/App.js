d7502618116d5795a96c22ac6e62840a9cc255bc
Arnix Minadeo <arnix676@gmail.com>
2023-02-10 16:47:57 -0300

    Fix: tag h1 dont found

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Mi primera aplicación</h1>
       <p>Esta es mi primera aplicación en React, está padrísimo!</p>
-    </>
+    </div>
   );
 }
 
